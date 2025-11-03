46c70ba6d4a293f022b91323d7802f4e67c90e37
ruth-mckeever <ruth.mckeever@dkit.ie>
2025-11-03 15:54:02 +0000

    All code hopefully

--- SanitizeService.cs
+++ SanitizeService.cs
@@ -1,6 +1,30 @@
+using Newtonsoft.Json;
+using RestSharp;
+
 namespace WFComments2025;
 
 public class SanitizeService
 {
-    
+    private static readonly string SanitizeURL = "http://www.purgomalum.com/service/";
+    public static string SanitizeTextJSON(string rawValue)
+    {
+        var client = new RestClient(SanitizeURL + "json");
+        var request = new RestRequest();
+
+        request.AddParameter("text", rawValue);
+        var response = client.Execute(request);
+        if (!string.IsNullOrWhiteSpace(response.Content))
+        {
+            string receivedJSON = response.Content;
+            PurgoMalumJSON? sanitizedResponse = JsonConvert.DeserializeObject<PurgoMalumJSON>(receivedJSON);
+            return sanitizedResponse.result;
+        }
+
+        return string.Empty;
+    }
 }
+public class PurgoMalumJSON
+{
+    public string result { get; set; }
+}
+
--- UserComment.cs
+++ UserComment.cs
@@ -1,6 +1,27 @@
+using System.Diagnostics.Contracts;
+
 namespace WFComments2025;
 
 public class UserComment
 {
-    
+    private string rawComment;
+    private string sanitizedComment;
+
+    public string RawComment
+    {
+        get { return rawComment; }
+        set { rawComment = value; }
+    }
+
+    public string SanitizedComment
+    {
+        get { return sanitizedComment; }
+        set { sanitizedComment = value; }
+    }
+
+    public UserComment(string rawComment)
+    {
+        this.rawComment = rawComment;
+        sanitizedComment = SanitizeService.SanitizeTextJSON(rawComment);
+    }
 }
